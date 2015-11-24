151c1e9704f85f932a06b9f07234f03f915c4f84
wesbasinger <wbasinger@villagetechschools.org>
2015-11-24 07:47:57 -0600

    Added a log to the __init__ of the Player class

--- Player.py
+++ Player.py
@@ -1,8 +1,14 @@
+import os
+
 class Player(object):
 
   def __init__ (self, name):
     self.name = name
     self.score = 0
+    path = '/Users/teacher/Desktop/LPTHW/Second_Half/class_practice/math_class_with_snakes/logs/'
+    filename = self.name + "log" + ".txt"
+    filename = os.path.join(path, filename)
+    self.log = open(filename, "w")
 
   def getscore(self):
     return self.score
