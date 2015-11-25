c92eec0a58752d2976f40a5cf0bc845f24fa960d
wesbasinger <wbasinger@villagetechschools.org>
2015-11-24 18:58:37 -0600

    Switched the data to binary.

--- Player.py
+++ Player.py
@@ -7,26 +7,26 @@ class Player(object):
     self.name = name # set player name
     self.score = 0 # initialize score to zero
     path = '/Users/teacher/Desktop/LPTHW/Second_Half/class_practice/math_class_with_snakes/logs'
-    filename = self.name + "_log.txt"
+    filename = self.name + "_log.bin"
     filename = os.path.join(path, filename)
-    f  = open( filename, "w")
+    f  = open( filename, "wb")
     f.write("0")
     f.close()
 
   def getscore(self):
     path = '/Users/teacher/Desktop/LPTHW/Second_Half/class_practice/math_class_with_snakes/logs'
-    filename = self.name + "_log.txt"
+    filename = self.name + "_log.bin"
     filename = os.path.join(path, filename)
-    f = open( filename, "r")
+    f = open( filename, "rb")
     print f.read()
     f.close()
 
   def setscore(self, points):
     self.score += points
     path = '/Users/teacher/Desktop/LPTHW/Second_Half/class_practice/math_class_with_snakes/logs'
-    filename = self.name + "_log.txt"
+    filename = self.name + "_log.bin"
     filename = os.path.join(path, filename)
-    f = open(filename, "w")
+    f = open(filename, "wb")
     f.write(str(self.score))
     f.close()
 """
